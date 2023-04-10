f6c6cbbd351037cd14e6c90b7d485e3579816092
Yankun Song <yankun.l.song@gmail.com>
2023-04-09 22:31:25 -0400

    added new link

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -6,6 +6,7 @@ export default function App() {
       <p>123</p>
       <Image src="https://ibb.co/TMfS1V4" alt="Example Image" />
       <Image src="https://i.ibb.co/GQ6L9KW/wechat.jpg" alt="wechat" />
+      <Image src="https://i.ibb.co/1Gg5MDr/wechat.jpg" alt="wechat2" />
     </>
   );
 }
