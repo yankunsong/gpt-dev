c1f9f95938ef74cab7ae3dd71ffeb8a91381ad6d
Yankun Song <yankun.l.song@gmail.com>
2023-04-09 21:51:50 -0400

    using Image

--- app/components/Support.tsx
+++ app/components/Support.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Support: React.FC = () => {
@@ -5,10 +6,10 @@ const Support: React.FC = () => {
     <div>
       <h1>Support</h1>
       <p>
-        We're here to help! If you have any questions or issues, please reach
+        We are here to help! If you have any questions or issues, please reach
         out to us at support@example.com.
       </p>
-      <img src="../../public/favicon-32x32.png" alt="Example Image" />
+      <Image src="../../public/favicon-32x32.png" alt="Example Image" />
     </div>
   );
 };
