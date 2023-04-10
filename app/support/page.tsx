969c4eab9c7aa0001078edbd7723ddb6222a09f6
Yankun Song <yankun.l.song@gmail.com>
2023-04-09 22:23:40 -0400

    added pic

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,7 +1,11 @@
+import Image from "next/image";
+
 export default function App() {
   return (
     <>
       <p>123</p>
+      <Image src="https://ibb.co/TMfS1V4" alt="Example Image" />
+      <Image src="https://i.ibb.co/GQ6L9KW/wechat.jpg" />
     </>
   );
 }
