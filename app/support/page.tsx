818f8b257f15e1781ac5d8dfaf875bc797b9546a
Yankun Song <yankun.l.song@gmail.com>
2023-04-09 22:37:13 -0400

    try

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -7,6 +7,11 @@ export default function App() {
       <Image src="https://ibb.co/TMfS1V4" alt="Example Image" />
       <Image src="https://i.ibb.co/GQ6L9KW/wechat.jpg" alt="wechat" />
       <Image src="https://i.ibb.co/1Gg5MDr/wechat.jpg" alt="wechat2" />
+      <Image
+        src="https://s1.imagehub.cc/images/2023/04/10/3816d1d91252c624d30bbd7102b140d0.jpeg"
+        alt="3816d1d91252c624d30bbd7102b140d0.jpeg"
+      />
+      <Image src="../../public/favicon-32x32.png" alt="Example Image" />
     </>
   );
 }
