28b4d2c101b2837d8be1aa4ab2c2add3ff520a67
Yankun Song <yankun.l.song@gmail.com>
2023-04-13 17:52:21 -0400

    added css

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -2,22 +2,26 @@
 
 import Image from "next/image";
 import React, { useState } from "react";
+import "./styles.scss";
 
 export default function App() {
   const [showText, setShowText] = useState(false);
   const handleButtonClick = () => {
     setShowText(!showText);
   };
+
   return (
-    <div style={{ display: "flex", flexWrap: "wrap" }}>
-      <div>
-        <p>Hello and welcome to my React app!</p>
-        <p>Thanks for checking it out!</p>
-        {showText && <p>Here is some hidden text that is now revealed!</p>}
-        <button onClick={handleButtonClick}>Show hidden text</button>
-      </div>
-      <div>
-        <Image src="wechat.jpeg" alt="123" />
+    <div>
+      <div className="container">
+        <div className="text-container">
+          <p>Hello and welcome to my React app!</p>
+          <p>Thanks for checking it out!</p>
+          {showText && <p>Here is some hidden text that is now revealed!</p>}
+          <button onClick={handleButtonClick}>Show hidden text</button>
+        </div>
+        <div className="image-container">
+          <Image src="wechat.jpeg" alt="123" className="image" />
+        </div>
       </div>
     </div>
   );
