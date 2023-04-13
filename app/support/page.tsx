a2739a951968e9b6cfae9e98d87631ea69b72842
Yankun Song <yankun.l.song@gmail.com>
2023-04-13 17:17:25 -0400

    add css

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -6,17 +6,12 @@ import React, { useState } from "react";
 export default function App() {
   const [showText, setShowText] = useState(false);
   const handleButtonClick = () => {
-    setShowText(true);
+    setShowText(!showText);
   };
   return (
-    <>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
       <div>
         <p>Hello and welcome to my React app!</p>
-        <p>网站运行至今，</p>
-        <p>
-          Here is another paragraph to demonstrate how multiple paragraphs can
-          be included in the same component.
-        </p>
         <p>Thanks for checking it out!</p>
         {showText && <p>Here is some hidden text that is now revealed!</p>}
         <button onClick={handleButtonClick}>Show hidden text</button>
@@ -24,6 +19,6 @@ export default function App() {
       <div>
         <Image src="wechat.jpeg" alt="123" />
       </div>
-    </>
+    </div>
   );
 }
