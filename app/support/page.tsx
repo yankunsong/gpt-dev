94f3bdb4822473ee04e14f3bf00528cb25aff7d5
Yankun Song <yankun.l.song@gmail.com>
2023-04-13 18:04:50 -0400

    added progess bar

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -9,19 +9,26 @@ export default function App() {
   const handleButtonClick = () => {
     setShowText(!showText);
   };
-
+  const A = 20;
+  const B = 140;
   return (
-    <div>
-      <div className="container">
-        <div className="text-container">
-          <p>Hello and welcome to my React app!</p>
-          <p>Thanks for checking it out!</p>
-          {showText && <p>Here is some hidden text that is now revealed!</p>}
-          <button onClick={handleButtonClick}>Show hidden text</button>
-        </div>
-        <div className="image-container">
-          <Image src="wechat.jpeg" alt="123" className="image" />
-        </div>
+    <div className="container">
+      <div className="text-container">
+        <p>Hello and welcome to my React app!</p>
+        <p>Thanks for checking it out!</p>
+      </div>
+      <div className="progress-container">
+        <p>
+          Progress: {A}/{B}
+        </p>
+        <progress value={A} max={B}></progress>
+      </div>
+      <div className="image-container">
+        <Image src="wechat.jpeg" alt="123" className="image" />
+      </div>
+      <div className="hidden-text-container">
+        {showText && <p>Here is some hidden text that is now revealed!</p>}
+        <button onClick={handleButtonClick}>Show hidden text</button>
       </div>
     </div>
   );
