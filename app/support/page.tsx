72dccb65bd9b70db6e356158b4fa05091a149856
Yankun Song <yankun.l.song@gmail.com>
2023-04-13 21:26:21 -0400

    added content

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -10,18 +10,29 @@ export default function App() {
     setShowText(!showText);
   };
   const A = 20;
-  const B = 20 * 7;
+  const B = 30 * 7;
   return (
     <div className="container">
       <div className="text-container">
-        <p>Hello and welcome to my React app!</p>
-        <p>Thanks for checking it out!</p>
+        <p>欢迎大家使用我的网站!</p>
+        <p>
+          目前为止，网站的建立和运营已经产生了200元的费用，并且每天还会新增十几元的消费。为了能长久地运行，现在开始网站将开始收费。大家可以根据自己的使用量，和GPT对自己的帮助程度，进行自助付费。
+        </p>
+        <p>
+          我大概每周会更新一次密码，强制大家回到这个页面。最新的密码会在页面底端。
+        </p>
+        <p>
+          另外，这个网站仅为星球朋友内部学习交流使用，请尽量不要分享给其他人。
+        </p>
+        <p>
+          如果使用中有任何问题，可以通过邮件与我联系。我的邮箱是bigcatisgreat@gmail.com.
+        </p>
       </div>
       <div className="progress-container">
         <p>
-          Progress: {A}/{B}
+          目前进度: {A}/{B}
         </p>
-        <progress value={A} max={B}></progress>
+        <progress id="my-progress-bar" value={A} max={B}></progress>
       </div>
       <div className="image-container">
         <Image src="wechat.jpeg" alt="123" className="image" />
